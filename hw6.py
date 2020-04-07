fea338010e817a14cf7490aa0f3c8727a7cff473
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-07 17:25:49 -0400

    added the file hw6.py

---
+++ hw6.py
@@ -0,0 +1,40 @@
+"""
+hw6.py
+Name(s):
+NetId(s):
+Date:
+"""
+
+import math
+import numpy
+import matplotlib.pyplot as plt
+
+"""
+FE: Forward Euler
+"""
+def FE(w0, z, m, w, x0, T, N):
+    return (x,t)
+
+"""
+BE: Backward Euler
+"""
+def BE(w0, z, m, w, x0, T, N):
+    return (x,t)
+
+"""
+CN: Crank-Nicolson
+"""
+def CN(w0, z, m, w, x0, T, N):
+    return (x,t)
+
+"""
+RK4: fourth order Runge-Kutta
+"""
+def RK4(w0, z, m, w, x0, T, N):
+    return (x,t)
+
+"""
+main
+"""
+if __name__ == '__main__':
+    pass
