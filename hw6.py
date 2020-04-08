71a4b6b4d14edc5e505b40c2ed6e188e96b18775
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-08 03:07:04 -0400

    Implemented FE method and tested for functionality

--- hw6.py
+++ hw6.py
@@ -13,28 +13,50 @@ import matplotlib.pyplot as plt
 FE: Forward Euler
 """
 def FE(w0, z, m, w, x0, T, N):
-    return (x,t)
+	A = numpy.matrix([[0,1],[-(w0**2),-2*z*w0]])	
+	dt = T/N
+
+	xV = [0]*(N+1)
+	xV[0]=x0
+
+	x = [0]*(N+1)
+	x[0] = x0.A[0][0]
+
+	t = [0]*(N+1)
+	for s in range(N+1):
+		t[s] = s*dt
+	for n in range(N):
+		b = numpy.matrix([[0],[math.cos(w*t[n])]])
+		xV[n+1] = xV[n]+ dt*A*xV[n]+dt*b
+		x[n+1]=xV[n+1].A[0][0]
+
+	return (x,t)
 
 """
 BE: Backward Euler
 """
 def BE(w0, z, m, w, x0, T, N):
-    return (x,t)
+	return (x,t)
 
 """
 CN: Crank-Nicolson
 """
 def CN(w0, z, m, w, x0, T, N):
-    return (x,t)
+	return (x,t)
 
 """
 RK4: fourth order Runge-Kutta
 """
 def RK4(w0, z, m, w, x0, T, N):
-    return (x,t)
+	return (x,t)
 
 """
 main
 """
 if __name__ == '__main__':
-    pass
+	print(FE(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
+
+
+
+
+
