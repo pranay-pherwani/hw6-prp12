57ea5e0b104eed7a2cdb65c74c93f0088d488a54
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-08 14:59:35 -0400

    Implemented RK4 function and tested it for functionality

--- hw6.py
+++ hw6.py
@@ -85,6 +85,34 @@ def CN(w0, z, m, w, x0, T, N):
 RK4: fourth order Runge-Kutta
 """
 def RK4(w0, z, m, w, x0, T, N):
+	A = numpy.matrix([[0,1],[-(w0**2),-2*z*w0]])	
+	dt = T/N
+
+	xV = [0]*(N+1)
+	xV[0]=x0
+
+	x = [0]*(N+1)
+	x[0] = x0.A[0][0]
+
+	t = [0]*(N+1)
+	for s in range(N+1):
+		t[s] = s*dt
+	for n in range(N):
+		b1 = numpy.matrix([[0],[math.cos(w*t[n])]])
+		k1 = dt*A*xV[n]+dt*b1
+
+		b2 = numpy.matrix([[0],[math.cos(w*(t[n]+dt/2))]])
+		k2 = dt*A*(xV[n]+k1/2)+dt*b2
+
+		b3 = numpy.matrix([[0],[math.cos(w*(t[n]+dt/2))]])
+		k3 = dt*A*(xV[n]+k2/2)+dt*b3
+
+		b4 = numpy.matrix([[0],[math.cos(w*(t[n]+dt))]])
+		k4 = dt*A*(xV[n]+k3)+dt*b4
+
+		xV[n+1] = xV[n] + k1/6 + k2/3 + k3/3 + k4/6
+		x[n+1]=xV[n+1].A[0][0]
+
 	return (x,t)
 
 """
@@ -97,6 +125,8 @@ if __name__ == '__main__':
 	print(BE(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
 	print('CN')
 	print(CN(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
+	print('RK4')
+	print(RK4(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
 
 
 
