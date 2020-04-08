a4f4ea9afeef57497c577b49a411ea10eed059c9
BlueAxolotl <pranay.pherwani@gmail.com>
2020-04-08 14:07:09 -0400

    Implemented BE function and tested it for functionality

--- hw6.py
+++ hw6.py
@@ -36,6 +36,23 @@ def FE(w0, z, m, w, x0, T, N):
 BE: Backward Euler
 """
 def BE(w0, z, m, w, x0, T, N):
+	A = numpy.matrix([[0,1],[-(w0**2),-2*z*w0]])	
+	dt = T/N
+
+	xV = [0]*(N+1)
+	xV[0]=x0
+
+	x = [0]*(N+1)
+	x[0] = x0.A[0][0]
+
+	t = [0]*(N+1)
+	for s in range(N+1):
+		t[s] = s*dt
+	for n in range(N):
+		b = numpy.matrix([[0],[math.cos(w*t[n+1])]])
+		xV[n+1] = (numpy.matrix([[1,0],[0,1]])-dt*A).I*(xV[n]+dt*b)
+		x[n+1]=xV[n+1].A[0][0]
+
 	return (x,t)
 
 """
@@ -54,7 +71,10 @@ def RK4(w0, z, m, w, x0, T, N):
 main
 """
 if __name__ == '__main__':
+	print('FE')
 	print(FE(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
+	print('BE')
+	print(BE(1,1,1,1,numpy.matrix([[0],[0]]),10,10))
 
 
 
